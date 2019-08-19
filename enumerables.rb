fdf5fa386e1399a536c75f320a8eea3515e1086f
matheus-fls <48728393+matheus-fls@users.noreply.github.com>
2019-08-19 17:18:46 -0300

    Added my_each method to enumerables file

---
+++ enumerables.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# :nodoc:
+module Enumerable
+  def my_each
+    my_array = to_a
+    cont = 0
+    while cont < length
+      yield(*my_array[cont])
+      cont += 1
+    end
+  end
+end
