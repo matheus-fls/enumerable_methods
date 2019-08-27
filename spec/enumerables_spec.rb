769dd4541b2917e81d09642413bb6ec430a812a9
matheus-fls <48728393+matheus-fls@users.noreply.github.com>
2019-08-27 13:42:13 -0300

    Added descriptive names for the tests

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -8,69 +8,69 @@ RSpec.describe Enumerable do
   let(:my_hash) { { one: 1, two: 2, three: 3, four: 4 } }
 
   describe 'my_select' do
-    it 'same as select with array' do
+    it 'Filters the values from an array according to a given condition' do
       expect(arr.my_select(&:odd?)).to eql(arr.select(&:odd?))
     end
 
-    it 'same as select with hash' do
+    it 'Filters the values from a hash according to a given condition' do
       expect(my_hash.my_select { |_key, value| value.odd? }).to eql(my_hash.select { |_key, value| value.odd? })
     end
   end
 
   describe 'my_all?' do
-    it 'same as all?' do
+    it 'Checks if all items in an array comply with a given condition' do
       expect(arr.my_all?(&:positive?)).to eql(arr.all?(&:positive?))
     end
 
-    it 'same as all?' do
+    it 'Checks if all items in a hash comply with a given condition' do
       expect(my_hash.my_all? { |_key, value| value.positive? }).to eql(my_hash.all? { |_key, value| value.positive? })
     end
   end
 
   describe 'my_any?' do
-    it 'same as any?' do
+    it 'Checks if any item in an array complies with a given condition' do
       expect(arr.my_any?(&:negative?)).to eql(arr.any?(&:negative?))
     end
 
-    it 'same as any?' do
+    it 'Checks if any item in a hash complies with a given condition' do
       expect(my_hash.my_any? { |_key, value| value.negative? }).to eql(my_hash.any? { |_key, value| value.negative? })
     end
   end
 
   describe 'my_count' do
-    it 'same as count for array' do
+    it 'Counts all the items in an array that complies with a given condition' do
       expect(arr.my_count(&:odd?)).to eql(arr.count(&:odd?))
     end
 
-    it 'same as count for hash' do
+    it 'Counts all the items in a hash that complies with a given condition' do
       expect(my_hash.my_count { |_key, value| value.odd? }).to eql(my_hash.count { |_key, value| value.odd? })
     end
 
-    it 'same as count for array' do
+    it 'Counts all the items in an array' do
       expect(arr.my_count).to eql(arr.count)
     end
 
-    it 'same as count for hash' do
+    it 'Counts all the items in a hash' do
       expect(my_hash.my_count).to eql(my_hash.count)
     end
   end
 
   describe 'my_map' do
-    it 'same as map for array' do
+    it 'Applies a block over an array and returns the result' do
       expect(arr.my_map { |x| x**2 }).to eql(arr.map { |x| x**2 })
     end
-    it 'same as map for hash' do
+    it 'Applies a block over a hash and returns the result' do
       expect(my_hash.my_map { |_, x| x**2 }).to eql(my_hash.map { |_, x| x**2 })
     end
   end
 
   describe 'my_inject' do
-    it 'same as inject for array' do
-      expect(arr.my_inject { |acc, x| 2 * acc + x }).to eql(arr.inject { |acc, x| 2 * acc + x })
+    it 'Calculates the sum of the squares of the numbers in the array' do
+      expect(arr.my_inject { |acc, x| acc + x**2 }).to eql(arr.inject { |acc, x| acc + x**2 })
     end
-    it 'same as inject for hash' do
-      expect(my_hash.my_inject { |acc, pair| acc[pair.last] = pair; acc }) # rubocop:disable Style/Semicolon
-        .to eql(my_hash.inject { |acc, pair| acc[pair.last] = pair; acc }) # rubocop:disable Style/Semicolon
+    it 'Converts a hash to an indexed list of key values' do
+      expect(my_hash.my_inject([]) { |acc, pair| acc[pair.last] = pair.first; acc }) # rubocop:disable Style/Semicolon
+        .to eql(my_hash.inject([]) { |acc, pair| acc[pair.last] = pair.first; acc }) # rubocop:disable Style/Semicolon
     end
   end
 end
